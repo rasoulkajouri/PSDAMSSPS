a37c642906018c26f238a3f38502156384c3105f
Rasoul <39953546+rasoulkajouri@users.noreply.github.com>
2019-05-21 00:38:03 +0430

    Delete allocation.cpp

--- allocation.cpp
+++
@@ -1,7 +0,0 @@
-#include "../include/masterHeader.h"
-
-void swimmer :: allocation(const int C, float* phaseTemp, float* orientationTemp){
-
-	phaseTemp [C] = phase;
-	orientationTemp [C] = orientation;
-}
